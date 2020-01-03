5e48fe96d81ab30b18f820640c5ea114c539d01a
rleongPd5 <rleong00@stuy.edu>
2020-01-03 03:46:54 -0500

    added description of assignment from site, going to test write now (pun intended hehehe)

--- write.c
+++ write.c
@@ -1,5 +1,9 @@
 #include "phone.h"
 
+// Will attempt to access the resources using the semaphore
+// Once in, it should display the last line added to the file (use shared memory to contain the size of the last line).
+// Prompt the user for the next line.
+// Once a new line is read, write that to the file, update the shared memory and then release the semaphore
 int main(){
   int shmd, semd;
   struct sembuf sb;
@@ -15,6 +19,18 @@ int main(){
   if (shmd < 0){
     printf("Error in write.c shmget: %s\n", strerror(errno));
   }
-  
+
+  int fd = open("telephone.txt", O_WRONLY | O_APPEND);
+  char *lastline = shmat(shmd, 0, 0); //attach shared memory to an available address (contain size of last line)
+  printf("last addition: %s\n", lastline);
+  char input[1024];
+  printf("your new addition: ");
+  fgets(input, 1024, stdin);
+  write(fd, input, strlen(input));
+  shmdt(input); //detach the segment
+  close(fd);
+  printf("\nthank you for your addition. it has been added!\n");
+  sb.sem_op = 1; //done using the resourse, so we have to up the semaphore
+  semop(semd, &sb, 1); //ups the semaphore
   return 0;
 }
