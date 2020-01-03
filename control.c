39361a9cbc77599d1f8614c207fa82644bf829d6
rleongPd5 <rleong00@stuy.edu>
2020-01-03 04:02:24 -0500

    added view and a print statement if no args are given

--- control.c
+++ control.c
@@ -22,33 +22,30 @@ int main(int argc, char * argv[]){
         semd = semget(KEY, 1, 0); //the 1 means that you want 1 semaphore
         v = semctl(semd, 0, GETVAL, 0); //gets the value of the semaphore.
         printf("semctl returned: %d\n", v);
-
+        return 1;
       }
+
       else{ //file didn't exist yet
 
         v = semctl(semd, 0, SETVAL, su);
         printf("semctl returned: %d\n", v); //value of semaphore
-        printf("semaphore created\n");
+        printf("semaphore created!\n");
 
         shmd = shmget(KEY, sizeof(char *), IPC_CREAT | IPC_EXCL | 0644);
         if (shmd < 0){
           printf("Error shmd: %s\n", strerror(errno));
+          return 1;
         }
-        printf("shared memory created\n");
+        printf("shared memory created!\n");
 
         int fd = open("telephone.txt", O_CREAT | O_TRUNC | O_RDWR, 0644);
         if (fd < 0){
           printf("Error in open in create: %s\n", strerror(errno));
+          return 1;
         }
         close(fd);
         printf("file 'telephone.txt' created!\n");
       }
-      // printf("shmd: %d\n, shmd);
-      //
-      // data = shmat(shmd, 0, 0);
-      //
-      // char[200] buffer;
-      // fgets(buffer,  200, stdin); //read in the user input
     }
     else if (!strcmp(argv[1], "-r" )){ //remove
       // Remove the shared memory, the semaphore and the story.
@@ -58,11 +55,13 @@ int main(int argc, char * argv[]){
         semd = semget(KEY, 1, 0);
         if (semd < 0){
           printf("Error semget in remove: %s\n", strerror(errno));
+          return 1;
         }
 
         int fd = open("telephone.txt", O_RDONLY); //removing doesn't require WR permissions
         if (fd < 0){
           printf("Error in open in remove: %s\n", strerror(errno));
+          return 1;
         }
 
         printf("The story so far: \n");
@@ -84,8 +83,25 @@ int main(int argc, char * argv[]){
         // Remove the segment (happens once, not once per process) -- shmctl (shared memory control)
     }
     else if (!strcmp(argv[1], "-v" )){ //view
-
+      // Output the contents of the story file.
+      // This mode does not need to interact with the semaphore
+      int fd = open("telephone.txt", O_RDONLY); //removing doesn't require WR permissions
+      if (fd < 0){
+        printf("Error in open in view: %s\n", strerror(errno));
+      }
+      printf("The story so far: \n");
+      char story[1024];
+      story[0] = '\0'; // trying to prevent seg fault by adding a null to terminate in case story is empty
+      read(fd, story, 1024);
+      if (strlen(story) != 0){ //try to get rid of seg fault error
+        *(strrchr(story, '\n')+ 1) = '\0'; //strrchr returns a pointer to last occurrence of a character, null terminate it
+      }
+      printf("%s\n", story);
+      close(fd);
     }
+  }
+  else{
+    printf("no argument given. please enter -c (create), -r (remove), or -v (view)\n", );
   }
   return 0;
 }
