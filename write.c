53d4ea02fc9c72b5f238a133c10794463b417339
rleongPd5 <rleong00@stuy.edu>
2020-01-03 03:40:01 -0500

    no more seg fault in control yay! starting to write write.c for further testing of control (by making a story)

--- write.c
+++ write.c
@@ -1,5 +1,20 @@
 #include "phone.h"
 
 int main(){
+  int shmd, semd;
+  struct sembuf sb;
+  sb.sem_num = 0;
+  sb.sem_op = -1;
+  printf("trying to get in...\n");
+  semd = semget(KEY, 1, 0);
+  if (semd < 0){
+    printf("Error in write.c semget: %s\n", strerror(errno));
+  }
+  semop(semd, &sb, 1); //down the semaphore
+  shmd = shmget(KEY, sizeof(char *), 0);
+  if (shmd < 0){
+    printf("Error in write.c shmget: %s\n", strerror(errno));
+  }
+  
   return 0;
 }
