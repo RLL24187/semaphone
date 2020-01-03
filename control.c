50c6451602fd6add3898bf7c21fcd0eb35f307c3
rleongPd5 <rleong00@stuy.edu>
2020-01-03 03:10:45 -0500

    attempting to remove the story, also moved all of the variable declarations out

--- control.c
+++ control.c
@@ -3,12 +3,18 @@
 
 int main(int argc, char * argv[]){
   if (argc > 1){
+    int *data;
+    int shmd, semd, v;
+    union semun su;
+    su.val = 1;
+    struct sembuf sb;
+    sb.sem-num = 0; //this is the semaphore number you want to use
+    //sb.sem_flg = SEM_UNDO;
+    sb.sem_op = -1; //downs the semaphore
     if (!strcmp(argv[1], "-c" ){ //create
       // If creating (command line argument: -c):
       // Should make the shared memory, semaphore and file (open the file with the truncate flag).
       // Set any values that are needed.
-      int *data;
-      int shmd, semd, v, r;
       // char input[3];
       semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);  //excl makes sure it's not created again
       if (semd < 0){ //file already exists
@@ -19,18 +25,12 @@ int main(int argc, char * argv[]){
 
       }
       else{ //file didn't exist yet
-        union semun su;
-        su.val = 1;
-        r = semctl(semd, 0, SETVAL, su);
-        printf("semctl returned: %d\n", r);
 
-        struct sembuf sb;
-        sb.sem-num = 0; //this is the semaphore number you want to use
-        //sb.sem_flg = SEM_UNDO;
-        sb.sem_op = -1; //downs the semaphore
-        printf("semaphore created\n", );
+        v = semctl(semd, 0, SETVAL, su);
+        printf("semctl returned: %d\n", v); //value of semaphore
+        printf("semaphore created\n");
 
-        shmd = shmget(KEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
+        shmd = shmget(KEY, sizeof(char *), IPC_CREAT | IPC_EXCL | 0644);
         if (shmd < 0){
           printf("Error shmd: %s\n", strerror(errno));
         }
@@ -38,7 +38,7 @@ int main(int argc, char * argv[]){
 
         int fd = open("telephone.txt", O_CREAT | O_TRUNC | O_RDWR, 0644);
         if (fd < 0){
-          printf("Error in open: %s\n", strerror(errno));
+          printf("Error in open in create: %s\n", strerror(errno));
         }
         close(fd);
         printf("file 'telephone.txt' created!\n");
@@ -51,7 +51,33 @@ int main(int argc, char * argv[]){
       // fgets(buffer,  200, stdin); //read in the user input
     }
     else if (!strcmp(argv[1], "-r" ){ //remove
+      // Remove the shared memory, the semaphore and the story.
+      // Display the full contents of the story.
+      // This should wait until the semaphore is available.
+        printf("trying to get in...\n");
+        semd = semget(KEY, 1, 0);
+        if (semd < 0){
+          printf("Error semget in remove: %s\n", strerror(errno));
+        }
 
+        int fd = open("telephone.txt", O_RDONLY); //removing doesn't require WR permissions
+        if (fd < 0){
+          printf("Error in open in remove: %s\n", strerror(errno));
+        }
+
+        printf("The story so far: \n");
+        char story[1024];
+        read(fd, story, 1024);
+        *(strrchr(story, '\n')+ 1 = '\0') //strrchr returns a pointer to last occurrence of a character, null terminate it
+        printf("%s\n", story);
+        close(fd);
+        // shmdt
+        shmctl(shmd,IPC_RMID, 0);
+        printf("shared memory deleted\n");
+        semctl(semd,IPC_RMID, 0);
+        printf("semaphore deleted\n");
+        // Detach the segment from a variable (once per process) -- shmdt (shared memory detach)
+        // Remove the segment (happens once, not once per process) -- shmctl (shared memory control)
     }
     else if (!strcmp(argv[1], "-v" ){ //view
 
